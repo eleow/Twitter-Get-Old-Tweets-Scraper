810e3d4f985d8794d44ea8044dc0fb1354ecbc2d
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2018-06-10 00:00:06 -0300

    rm hist from setup

--- setup.py
+++ setup.py
@@ -5,11 +5,8 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 requirements = [
     "pyquery>=1.2.17",
